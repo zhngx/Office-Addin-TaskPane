fc92db49d2936ba2ef4ca671fb9e356719a70b8d
Zheng Xu <zhengx@microsoft.com>
2019-01-30 15:58:06 -0800

    remove office-js-helper and jquery and move fonts to root directory

--- src/taskpane/taskpane.ts
+++ src/taskpane/taskpane.ts
@@ -3,16 +3,13 @@
  * See LICENSE in the project root for license information.
  */
 
-import * as OfficeHelpers from "@microsoft/office-js-helpers";
-
-$(document).ready(() => {
-  $("#run").click(run);
-});
+// import * as OfficeHelpers from './utility';
+document.getElementById('run').onclick = run;
 
 // The initialize function must be run each time a new page is loaded
 Office.initialize = () => {
-  $("#sideload-msg").hide();
-  $("#app-body").show();
+  document.getElementById('sideload-msg').style.display='none';
+  document.getElementById('app-body').style.display='flex';
 };
 
 async function run() {
@@ -33,7 +30,6 @@ async function run() {
       console.log(`The range address was ${range.address}.`);
     });
   } catch (error) {
-    OfficeHelpers.UI.notify(error);
-    OfficeHelpers.Utilities.log(error);
+    console.error(error);
   }
 }
